4cf23f0f99662199313f0085a276452c68338ae6
jmot4 <jmota@gtech-services.com>
2026-02-04 15:51:26 -0600

    Se agrega ordenamiento burbuja, se ordenan carpetas y se inicia pantalla de registro de alumnos

---
+++ Calculadora/Formularios/frmRegistro.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Calculadora.Formularios
+{
+    partial class frmRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            txtNombre = new TextBox();
+            label2 = new Label();
+            txtApellido = new TextBox();
+            label3 = new Label();
+            dtpFecha = new DateTimePicker();
+            btnRegistrar = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(794, 451);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnRegistrar);
+            tabPage1.Controls.Add(dtpFecha);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(txtApellido);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(txtNombre);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(786, 413);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Registros";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(786, 413);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Mostrar";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre";
+            label1.Click += label1_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(22, 59);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(398, 31);
+            txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Apellidos";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(31, 158);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(389, 31);
+            txtApellido.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Fecha de Nacimiento";
+            // 
+            // dtpFecha
+            // 
+            dtpFecha.Location = new Point(31, 253);
+            dtpFecha.Name = "dtpFecha";
+            dtpFecha.Size = new Size(300, 31);
+            dtpFecha.TabIndex = 5;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(654, 360);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(112, 34);
+            btnRegistrar.TabIndex = 6;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // frmRegistro
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "frmRegistro";
+            Text = "Registro Alumnos";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private TabPage tabPage2;
+        private TextBox txtNombre;
+        private Button btnRegistrar;
+        private DateTimePicker dtpFecha;
+        private Label label3;
+        private TextBox txtApellido;
+        private Label label2;
+    }
+}
